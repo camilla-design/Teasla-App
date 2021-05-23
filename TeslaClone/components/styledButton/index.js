094b8f2abf5a181cca1565c503104a032f12556c
Atlungstad Brenneri <camilla_repp@hotmail.com>
2021-05-23 12:25:44 +0200

    done

--- TeslaClone/components/styledButton/index.js
+++ TeslaClone/components/styledButton/index.js
@@ -2,22 +2,21 @@ import React from 'react';
 import {View, Text, Pressable} from 'react-native';
 import styles from './style';
 
-const StyledButton = (props) => {
-    
-    const { type, content, onPress } = props;
+const StyledButton = props => {
+  const {type, content, onPress} = props;
 
-    const backgroundColor = type === 'primary' ? '#171A20CC' : '#FFFFFFA6';
-    const textColor = type === 'primary' ? '#FFFFFF' : '#171A20';
+  const backgroundColor = type === 'primary' ? '#171A20CC' : '#FFFFFFA6';
+  const textColor = type === 'primary' ? '#FFFFFF' : '#171A20';
 
-    return(
-        <View style={styles.container}>
-            <Pressable
-            style={[styles.button, {backgroundColor: backgroundColor}]}
-            onPress={() => onPress()}>
-                <Text style={[styles.text, {color: textColor}]}>{content}</Text>
-            </Pressable>
-      </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Pressable
+        style={[styles.button, {backgroundColor: backgroundColor}]}
+        onPress={() => onPress()}>
+        <Text style={[styles.text, {color: textColor}]}>{content}</Text>
+      </Pressable>
+    </View>
+  );
 };
 
 export default StyledButton;
